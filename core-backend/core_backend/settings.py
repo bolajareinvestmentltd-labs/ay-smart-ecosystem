e6d1aa32df8dc719e36bdadc158373d981218c4e
Jc-Ls <jareschoicelabs@gmail.com>
2026-07-24 22:54:56 +0100

    Clean config for fresh Render root directory deployment

--- core-backend/core_backend/settings.py
+++ core-backend/core_backend/settings.py
@@ -131,3 +131,9 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# --- Render Static Files & Security Configuration ---
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
